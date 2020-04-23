9af3e9f901ff502a76f68d3cc30edbabdc0856fb
Rayni Nuñez <timetopwm@gmail.com>
2020-04-22 23:52:47 -0400

    Se ha instalado Styled-Components, se ha creado un boton y se le ha dado estilo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-// import logo from './logo.svg';
+import styled from 'styled-components';
+
+const Boton = styled.button`
+
+  background:-webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: 'Ariel', Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
+const Contenedor = styled.div`
+
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+
+`;
 
 function App() {
-  return (
-    
-    <h1> Breaking bad </h1>
 
+  const consultarApi = () => {
+    console.log("Con el lapiz no tu maldita madree");
+  }
+
+
+
+
+
+  return (
+
+    <Contenedor>
+
+        <Boton
+          onClick={consultarApi}
+        >
+
+          Obtener frase
+
+        </Boton>
+
+    </Contenedor>
   );
 }
 
